cc77d1a18721e570e8b75c0ebc4fdaa4824b17f4
w4r-h0und <d6leob6m6@gmail.com>
2024-09-15 23:09:54 -0400

    modified:   install.sh 	modified:   .tmux/plugins/tmux (modified content) 	modified:   .tmux/plugins/tmux-continuum (modified content) 	modified:   .tmux/plugins/tmux-mullvad (modified content) 	modified:   .tmux/plugins/tmux-open (modified content) 	modified:   .tmux/plugins/tmux-prefix-highlight (modified content) 	modified:   .tmux/plugins/tmux-resurrect (modified content) 	modified:   .tmux/plugins/tmux-sensible (modified content) 	modified:   .tmux/plugins/tmux-yank (modified content) 	modified:   .tmux/plugins/tpm (modified content) 	modified:   .tmux/plugins/vim-tmux-navigator (modified content)

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-# run this to clear out the terminal errors you'll get after stowing.
+# run this script to clear out the terminal errors you'll get after stowing the dotfiles directory
+
+
 OS=$(lsb_release -is)
 
 
 if [[ $OS == "Kali" || $OS == "Ubuntu" ]]; then
   echo "Installing for $OS..."
   sudo apt update
-  sudo apt install -y eza zoxide fastfetch
+  sudo apt install -y eza zoxide fastfetch ripgrep
 elif [[ $OS == "Arch" ]]; then
   echo "Installing for $OS..."
-  sudo pacman -Syu eza zoxide fastfetch
+  sudo pacman -Syu eza zoxide fastfetch ripgrep
 else
   echo "Unsupported OS: $OS"
   exit 1
 fi
 
+
 #Starship
 echo "Installing others...."
+wait 1;
+#FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+#Starship
 curl -sS https://starship.rs/install.sh | sh
 # Oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
@@ -26,4 +34,5 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 wait 1;
 zplug install
 
+source ~/.zshrc
 
